d3dabacb7d6c05a058f733d694f82e4c98c1f150
Sagar <53867930+the-e3n@users.noreply.github.com>
2020-11-26 00:16:11 +0530

    Update settings.py

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'npm5mc8ntczjn0wzf9i^@^i=gebgw_^(!c=etuaija+=12(%18'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
