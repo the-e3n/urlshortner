7de65234a144c5c152857cebaa184ada4b242938
Sagar <53867930+the-e3n@users.noreply.github.com>
2020-11-26 11:15:17 +0530

    Update urls.py

--- urlshortner/urls.py
+++ urlshortner/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from shortner.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('create/', create),
+    path('list', list),
     path('', include('shortner.urls')),
+
 ]
