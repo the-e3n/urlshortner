69e37d5ea1101c352741bbfd479260b3208e9c99
Sagar <53867930+the-e3n@users.noreply.github.com>
2020-11-26 11:16:02 +0530

    Update urls.py

--- shortner/urls.py
+++ shortner/urls.py
@@ -3,8 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('create/',views.create),
-    path('go/<_url>',views.index),
-    path('',views.index2),
-    path('list/',views.list_short),
+    path('<_url>',views.index),
 ]
